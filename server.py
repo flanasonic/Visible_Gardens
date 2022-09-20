39b4ea8190c839108f0ae5423d37b4f238adf763
flanasonic <flanasonic@gmail.com>
2022-09-19 22:43:08 -0400

    added first Flask route with simple database querye

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-"""Server for movie ratings app."""
-
-from flask import Flask
+from model import db, Company
+from flask import Flask, render_template
 
 DB_URI = 'postgresql:///indoorfarms'
 # TODO: make a config for database URI
@@ -8,30 +7,32 @@ DB_URI = 'postgresql:///indoorfarms'
 # create a Flask object and call it "app" 
 app = Flask(__name__)
 
+# tells our app where to find the db
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+# prints our SQL commands to Python terminal, can shut off when needed
+app.config["SQLALCHEMY_ECHO"] = False
+# need to include this line -set to False, otherwise it will waste memory
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 
-# Replace this with routes and view functions!
+# let's 'glue' our Flask app (named 'app') to our 
+# Flask SQLAlchemy object (named 'db')
+# and tell the app to start running with db
+db.init_app(app)
+
+
+
+
+
+@app.get("/")
+def get_main_page():
+    companies = Company.query.all()
+    return render_template('main.html', companies=companies)
 
 
 if __name__ == "__main__":
-
     app.run(host="0.0.0.0", debug=True)
-
-    # now call function connect_to_db() to connect our Flask app (called 'app')
-    # to our database called 'indoorfarms' 
     
-    # tells our app where to find the db
-    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
-    # prints our SQL commands to Python terminal, can shut off when needed
-    app.config["SQLALCHEMY_ECHO"] = False
-    # need to include this line -set to False, otherwise it will waste memory
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 
-
-    # let's 'glue' our Flask app (named 'app') to our 
-    # Flask SQLAlchemy object (named 'db')
-    # and tell the app to start running with db
-    from model import db
-    db.init_app(app)
 
 
 
