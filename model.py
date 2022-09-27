a4899563735a6a2e1307eb907069aec5705126df
flanasonic <flanasonic@gmail.com>
2022-09-26 21:30:23 -0400

    updated data model

--- model.py
+++ model.py
@@ -6,6 +6,9 @@ db = SQLAlchemy()
 
 #####################################################################
 # Company
+# TODO: !!!!! make it so that each company has just one default address, I 
+# think this is the address_id we want to put in 'address_id'!!!!
+
 #####################################################################
 
 class Company(db.Model):
@@ -16,25 +19,27 @@ class Company(db.Model):
     trade_name = db.Column(db.String(50))
     legal_name = db.Column(db.String(50))
     website = db.Column(db.String(50))
-    year_founded = db.Column(db.Integer)
+    address_id = db.Column(db.Integer) # default address?
     country = db.Column(db.String(50))
     parent_id = db.Column(db.Integer)
+    year_founded = db.Column(db.Integer)
     statement = db.Column(db.String(1000))
     total_employees = db.Column(db.Integer)
     legal_form = db.Column(db.String(50))
     for_profit = db.Column(db.Boolean, default=True)
     ownership = db.Column(db.String(50))
     business_focus = db.Column(db.String(75), default='specialty crop grower')
-    address_id = db.Column(db.Integer)
    
    # Declarative Table Configuration
    # https://docs.sqlalchemy.org/en/14/orm/declarative_tables.html#declarative-table-configuration
     __table_args__ = (
         db.ForeignKeyConstraint(
             # local field   # foreign (table.column)
-            ['address_id'], ['address.id'], name="fk_product_address"),
+            # ['address_id'], ['address.id'], name="fk_product_address")
+            ['address_id'], ['address.id'], name="fk_company_address"),
     )
 
+    # TODO: still not clear on how these work...
     # Company properties
     #   - SQLAlchemy will fill these in with objects when querying
     #   - We will populate these when "seeding" our database
@@ -56,16 +61,61 @@ class Address(db.Model):
     __tablename__ = 'address'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # company_id = db.Column(db.Integer) # TODO: do we need this?
+    # facility_id = db.Column(db.Integer) # TODO: do we need this?
+    make_default = db.Column(db.Boolean, default=False) # TODO: I think we need this?
+    # has_facility = db.Column(db.Boolean, default=False) # TODO: do we need this?
     address_1 = db.Column(db.String(75))
     address_2 = db.Column(db.String(75))
     suite = db.Column(db.String(50))
     city = db.Column(db.String(50))
     state = db.Column(db.String(10))
-    zip = db.Column(db.String(10))
+    postal = db.Column(db.String(10))
     country = db.Column(db.String(50))
+
+    # Address properties
+    #   TODO: do we need to give Address a company property?
+    # company = db.relationship("Company")
 
     def __repr__(self):
         return " ".join( [f"({name}: {getattr(self, name)})" for name in self.__table__.columns.keys()] )
+
+
+#####################################################################
+# Facility
+#####################################################################
+
+class Facility(db.Model):
+
+    __tablename__ = 'facility'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    company_id = db.Column(db.Integer)
+    nickname = db.Column(db.String(75))
+    type = db.Column(db.String(75))
+    year_opened = db.Column(db.Integer)
+    address_id = db.Column(db.Integer)
+    facility_employees = db.Column(db.Integer)
+
+   # Declarative Table Configuration - SQLALchemy documentation: 
+   # https://docs.sqlalchemy.org/en/14/orm/declarative_tables.html#declarative-table-configuration
+    __table_args__ = (
+        db.ForeignKeyConstraint(
+            # local field   # foreign (table.column)
+            ['address_id'], ['address.id'], name='fk_facility_address'),
+        db.ForeignKeyConstraint(
+            # local field   # foreign (table.column)
+            ['company_id'], ['company.id'], name='fk_facility_company')    )
+
+    # set up relationship between class Facility and class Address
+    # a facility has one address, an address has many facilities
+    address = db.relationship("Address")
+
+    def __repr__(self):
+        """Show info about facility."""
+
+        return f'<Facility id={self.id} name={self.name}>'
+
 
 #####################################################################
 # Product
@@ -95,41 +145,3 @@ class Product(db.Model):
             ['company_id'], ['company.id'], name="fk_product_company"),
     )
 
-
-#####################################################################
-# Facility
-#####################################################################
-
-class Facility(db.Model):
-
-    __tablename__ = 'facility'
-    
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    company_id = db.Column(db.Integer)
-    nickname = db.Column(db.String(75))
-    is_farm = db.Column(db.String(15))
-    is_office = db.Column(db.String(15))
-    is_country_hq = db.Column(db.String(15))
-    is_other_type = db.Column(db.String(25))
-    year_opened = db.Column(db.Integer)
-    address_id = db.Column(db.Integer)
-    facility_employees = db.Column(db.Integer)
-
-   # Declarative Table Configuration - SQLALchemy documentation: 
-   # https://docs.sqlalchemy.org/en/14/orm/declarative_tables.html#declarative-table-configuration
-    __table_args__ = (
-        db.ForeignKeyConstraint(
-            # local field   # foreign (table.column)
-            ['address_id'], ['address.id'], name='fk_facility_address'),
-        db.ForeignKeyConstraint(
-            # local field   # foreign (table.column)
-            ['company_id'], ['company.id'], name='fk_facility_company')    )
-
-    # set up relationship between Facility and Address classes
-    # a facility has one address, an address has many facilities
-    address = db.relationship("Address")
-
-    def __repr__(self):
-        """Show info about facility."""
-
-        return f'<Facility id={self.id} name={self.name}>'
